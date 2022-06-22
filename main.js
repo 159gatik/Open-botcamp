33faee848f7fe992afc9dc416c13c7e0e1eec7a4
159gatik <rojasgaston95@gmail.com>
2022-06-22 20:15:09 -0300

    entrega 2

---
+++ main.js
@@ -0,0 +1,16 @@
+let nombre = "Gaston";
+
+let edad = 24;
+
+let desarrollador = false;
+
+let fechaNacimiento = new Date(2022, 8, 22);
+
+let libroFavorito = {
+    titulo: "Un punto azul pálido",
+    autor: "C.Sagan",
+    fecha: 1994,
+    url: "https://astroaventura.net/wp-content/uploads/2020/09/Un-Punto-Azul-Palido.pdf",
+}
+
+const lista = [nombre, edad, desarrollador, fechaNacimiento, libroFavorito];

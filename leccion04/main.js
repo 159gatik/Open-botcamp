38e6ec687b3d3a6445fec80268181a02105c3d13
159gatik <rojasgaston95@gmail.com>
2022-06-24 15:47:51 -0300

    se realiza el ejercicio 4, string

---
+++ leccion04/main.js
@@ -0,0 +1,28 @@
+// Cadena de texto con nombre
+let nombre = "Gastón";
+
+// Cadena de texto con apellido
+let apellido = "Rojas";
+
+// Cadena de texto concatenando
+let estudiante = nombre.concat(" ", apellido);
+console.log(estudiante);
+
+// Cadena de texto con mayuscula y minuscula
+let estudianteMayus = estudiante.toUpperCase();
+let estudianteMinus = estudiante.toLowerCase();
+
+// Variable que contiene el numero de letras
+let letrasNum = estudiante.length;
+
+// Variable que contiene la primer letra del nombre y ultima del apellido.
+let primerLetra = nombre.charAt("0");
+let ultimaLetra = apellido.substring("4");
+
+// Cadena de texto que elimina los espacios
+let cadena = estudiante.replace(/ /g, "");
+console.log(cadena);
+
+//Variable bolleana que verifica el contenido
+let palabra = estudiante.includes(nombre);
+console.log(palabra);

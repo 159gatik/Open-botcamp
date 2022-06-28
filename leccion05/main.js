4e3101ebebea051fa08a02ba28e3d654cd080d29
159gatik <rojasgaston95@gmail.com>
2022-06-28 17:15:43 -0300

    se realiza ejercicio 5, numeros en js

--- leccion05/main.js
+++ leccion05/main.js
@@ -1,4 +1,19 @@
 let altura = 169;
 
-let alturaMts = perseFloat(`1,69`);
-console.log(alturaMts);
+let alturaMts = '1.69';
+console.log(parseFloat(alturaMts));
+
+
+let pesoKg = '71.80';
+console.log(parseFloat(pesoKg));
+
+
+let redondeoArriba = Math.round(alturaMts);
+
+console.log(redondeoArriba);
+
+let redondeoAbajo = Math.floor(pesoKg);
+
+console.log(redondeoAbajo);
+
+let valor = Number.MAX_VALUE + 1 === Number.MAX_VALUE;

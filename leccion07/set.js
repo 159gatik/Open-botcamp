71a59008a29c15a77a6c318c51d5500ea8efa8af
159gatik <rojasgaston95@gmail.com>
2022-07-24 11:20:57 -0300

    ejercicio set

---
+++ leccion07/set.js
@@ -0,0 +1,13 @@
+const set = ["Mariano", "Maria", "Jorge", "Nadia", "Gaston"]
+
+const miSet = new Set(set)
+
+console.log(miSet);
+
+miSet.add("Gaston")
+
+console.log(miSet);
+
+miSet.add("JavaScript")
+
+console.log(miSet);
